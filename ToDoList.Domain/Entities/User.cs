11d0aa604b31245ddcf8e7e2207dd52dcf1e613b
Roman Savenko <rsaven.wk@gmail.com>
2023-07-19 23:28:29 +0300

    refactor: update user validation

--- ToDoList.Domain/Entities/User.cs
+++ ToDoList.Domain/Entities/User.cs
@@ -6,20 +6,22 @@ namespace ToDoList.Domain.Entities;
 public class User
 {
     public long Id { get; set; }
-    [Required]
+    [Display(Name = "User name")]
+    [Required(ErrorMessage = "The User name is required")]
     [StringLength(20)]
     public string UserName { get; set; } = String.Empty;
-    [Required]
+    [Required(ErrorMessage = "The Email is required")]
     [DataType(DataType.EmailAddress)]
     public string Email { get; set; } = String.Empty;
-    [Required]
+    [Required(ErrorMessage = "The Password is required")]
     [MinLength(4)]
     [DataType(DataType.Password)]
     public string Password { get; set; } = String.Empty;
-    [Required]
-    [MinLength(4)]
+    [Display(Name = "Confirm password")]
+    [Required(ErrorMessage = "The Confirm password is required")]
+    [MinLength(4, ErrorMessage = "The min length of password is 4")]
     [DataType(DataType.Password)]
-    [Compare("Password", ErrorMessage = "")]
+    [Compare("Password")]
     public string ConfirmPassword { get; set; } = String.Empty;
     public ICollection<Task>? Tasks { get; set; }
 }
